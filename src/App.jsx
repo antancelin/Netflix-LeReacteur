27b01f0425074a7afdf1d1494b2cc7dc457dc0d5
Antoine Ancelin <ancelin.ant@gmail.com>
2024-11-13 21:36:06 +0100

    added footer and added media query for mobile format

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,22 @@ function App() {
           <Section data={dataTab} />
         </main>
       </div>
+      <footer>
+        <p>
+          Made with{" "}
+          <span>
+            <a href="https://fr.react.dev/">React</a>
+          </span>{" "}
+          at{" "}
+          <span>
+            <a href="https://www.lereacteur.io/">Le Reacteur</a>
+          </span>{" "}
+          By{" "}
+          <span>
+            <a href="https://github.com/antancelin"> Antoine Ancelin</a>
+          </span>
+        </p>
+      </footer>
     </>
   );
 }
